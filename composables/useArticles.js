17024ddc74de7f3fb57cf289f16e296be7106a6d
Weixin Zheng <zhengweixin0101@outlook.com>
2025-08-21 07:36:52 +0800

    api请求加密

--- composables/useArticles.js
+++ composables/useArticles.js
@@ -2,25 +2,30 @@ import { ref } from 'vue'
 import axios from 'axios'
 
 const API_BASE = 'https://article.api.zhengweixin.top/api'
+const API_SECRET = 'q%t}o0B6+aLxCkMKEBhL}RujiWgH}?XWD%rn.KPVHNnr:9_=Z^Y:1.N:-5Le9=3CYrc3WA%Tdv*Yp**LcA*-9}e5:#Wee4Lr,%m3'
 
 export function useArticles() {
     const articles = ref([])
 
     // 获取文章列表
     const fetchArticles = async () => {
-        const res = await axios.get(`${API_BASE}/list?posts=all`)
+        const res = await axios.get(`${API_BASE}/list?posts=all`, {
+            headers: { 'x-api-key': API_SECRET }
+        })
         articles.value = res.data
     }
 
     // 获取文章内容
     const getArticle = async (slug) => {
-        const res = await axios.get(`${API_BASE}/article`, { params: { slug } })
+        const res = await axios.get(`${API_BASE}/article`, {
+            headers: { 'x-api-key': API_SECRET },
+            params: { slug }
+        })
         return res.data
     }
 
     // 编辑或创建文章
     const saveArticle = async (article) => {
-        // 只保留有值的字段
         const payload = {}
         if (article.slug) payload.slug = article.slug
         if (article.title) payload.title = article.title
@@ -30,13 +35,18 @@ export function useArticles() {
         if (article.tags && article.tags.length > 0) payload.tags = article.tags
         if (typeof article.published === 'boolean') payload.published = article.published
 
-        const res = await axios.put(`${API_BASE}/edit`, payload)
+        const res = await axios.put(`${API_BASE}/edit`, payload, {
+            headers: { 'x-api-key': API_SECRET }
+        })
         return res.data
     }
 
     // 删除文章
     const deleteArticle = async (slug) => {
-        await axios.delete(`${API_BASE}/delete`, { data: { slug } })
+        await axios.delete(`${API_BASE}/delete`, {
+            headers: { 'x-api-key': API_SECRET },
+            data: { slug }
+        })
     }
 
     return { articles, fetchArticles, getArticle, saveArticle, deleteArticle }
