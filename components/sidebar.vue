6ba4b0d28c71397bdd551937df1c0c6ebb696216
Weixin Zheng <zhengweixin0101@outlook.com>
2025-10-03 10:51:19 +0800

    调整页面结构

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -4,8 +4,8 @@
       <h2 class="text-xl font-bold mb-4">管理后台</h2>
       <nav class="flex flex-col gap-2">
         <NuxtLink to="/" class="hover:text-blue-500">首页</NuxtLink>
-        <NuxtLink to="/list" class="hover:text-blue-500">文章列表</NuxtLink>
-        <NuxtLink to="/create" class="hover:text-blue-500">创建文章</NuxtLink>
+        <NuxtLink to="/articles" class="hover:text-blue-500">文章列表</NuxtLink>
+        <NuxtLink to="/articles/create" class="hover:text-blue-500">创建文章</NuxtLink>
         <NuxtLink to="/talks" class="hover:text-blue-500">说说管理</NuxtLink>
         <NuxtLink to="/images" class="hover:text-blue-500">图片管理</NuxtLink>
       </nav>
