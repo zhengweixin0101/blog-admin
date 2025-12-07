6b0d788e28227c700636121c3cc1066c8c62707b
Weixin Zheng <zhengweixin0101@outlook.com>
2025-12-07 08:35:19 +0800

    微调

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <aside class="fixed top-0 left-0 w-20 md:w-40 bg-gray-100 h-screen p-4">
-      <h2 class="text-xl font-bold mb-4">管理后台</h2>
+      <h2 class="text-xl font-bold mb-4">博客后台</h2>
       <nav class="flex flex-col gap-3">
         <NuxtLink to="/" class="rounded p-2 transition-colors no-underline" 
-                   :class="$route.path === '/' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black hover:text-blue-500'">首页</NuxtLink>
+                   :class="$route.path === '/' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black hover:text-blue-500'">首页概览</NuxtLink>
         <NuxtLink to="/articles" class="rounded p-2 transition-colors no-underline" 
                    :class="$route.path.startsWith('/articles') && $route.path !== '/articles/create' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black hover:text-blue-500'">文章列表</NuxtLink>
         <NuxtLink to="/articles/create" class="rounded p-2 transition-colors no-underline" 
